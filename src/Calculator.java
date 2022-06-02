8fa7d3ed8171692ed53f0d35d3cc489367bbc3e2
Ala <alicja.lazarsak5@gmail.com>
2022-06-02 16:41:12 +0200

    new class - Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 1;
+        int secondNumber = 2;
+        float thirdNumber = 2.5f;
+
+        float result = secondNumber * thirdNumber;
+        System.out.println(result);
+    }
+}
