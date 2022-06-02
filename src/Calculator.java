e8ca4756800974548ad32229ee4452b22c69e898
Ala <alicja.lazarsak5@gmail.com>
2022-06-02 18:03:43 +0200

    Calculator - new functions

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args) {
 
-        int firstNumber = 1;
-        int secondNumber = 2;
-        float thirdNumber = 2.5f;
+        Scanner numberScanner = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę: ");
+        int firstNumber = numberScanner.nextInt();
+        System.out.println("Podaj drugą liczbę: ");
+        int secondNumber = numberScanner.nextInt();
 
-        float result = secondNumber * thirdNumber;
-        System.out.println(result);
+        //wyniki
+        System.out.println("Wynik dodawania: " + (firstNumber + secondNumber));
+        System.out.println("Wynik odejmowania: " + (firstNumber - secondNumber));
+        System.out.println("Wynik mnożenia: " + (firstNumber * secondNumber));
+        System.out.println("Wynik dzielenia: " + (firstNumber / secondNumber));
+        System.out.println("Wynik modulo: " + (firstNumber%secondNumber));
+
+
     }
 }
