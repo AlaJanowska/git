5ea69fa6364ee1e49f3aad9fb5b2f92608a9720a
Ala <alicja.lazarsak5@gmail.com>
2022-06-02 17:31:05 +0200

    new class - Calculator

--- src/CustomScanner.java
+++ src/CustomScanner.java
@@ -1,13 +1,22 @@
+import javax.print.attribute.standard.PresentationDirection;
+import java.time.ZonedDateTime;
 import java.util.Scanner;
 
 public class CustomScanner {
 
     public static void main(String[] args) {
 
+        //welcome
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj swoje imię:");
         String name = scanner.nextLine();
-        System.out.println( "Cześć " + name + ". Miło mi Cię poznać!");
+        System.out.println( "Cześć " + name + " Miło mi Cię poznać!");
+
+        //calculator
+        System.out.println("Podaj rok swojego urodzenia:");
+        int year = scanner.nextInt();
+        int result = ZonedDateTime.now().getYear() - year;
+        System.out.println("Masz " + result + "lat!");
 
     }
 }
