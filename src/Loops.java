218176dee16887a66b08b2bc3e1f37acb1c57d41
Ala <alicja.lazarsak5@gmail.com>
2022-06-03 23:30:58 +0200

    add class - Loops

---
+++ src/Loops.java
@@ -0,0 +1,31 @@
+public class Loops {
+
+    public static void main(String[] args) {
+
+        /**for loop */
+//        for (int i=0; i<=10; i++){
+//            System.out.println(i);
+//        }
+
+//        for (int i=0; i<100; i++){
+//            if (i%5 ==0){
+//                System.out.println(i);
+//            }
+//        }
+
+        /** while loop */
+//        int number = 10;
+//
+//        while (number < 10){
+//            System.out.println("liczba " + number + " jest mniejsza od 10");
+//            number++;
+//        }
+
+        /** do while loop */
+        int number = 15;
+        do {
+            System.out.println("liczba " + number + " jest mniejsza od 10");
+            number++;
+        }while (number < 10);
+    }
+}
